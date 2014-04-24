eff3f719004d2da485f8ad013613dfe251605058
Jed Barlow <jed@jedbarlow.ca>
2014-04-24 12:57:17 -0600

    cleanup whitespace; change arc direction and reference to clockwise from upward

--- RpkmVisualizer/src/com/biol498/rpkmvisualizer/VisualizerPanel.java
+++ RpkmVisualizer/src/com/biol498/rpkmvisualizer/VisualizerPanel.java
@@ -45,16 +45,16 @@ public class VisualizerPanel extends JComponent {
         ArrayList<Integer> a;
         maxMagnitude = 0;
         for(int i = 0; i < 100; i++) {
-        	a = new ArrayList<Integer>(2);
-        	a.add(spot); // Start
-        	spot += r.nextInt(1000);
-        	a.add(spot); // End
-        	spot += 1;
-        	a.add((int)(100 * Math.pow(r.nextDouble(), 3))); // Amount
-        	if(a.get(2) > maxMagnitude)
-        		maxMagnitude = a.get(2);
-        	a.add(r.nextInt(100000));
-        	randomData.add(a);
+            a = new ArrayList<Integer>(2);
+            a.add(spot); // Start
+            spot += r.nextInt(1000);
+            a.add(spot); // End
+            spot += 1;
+            a.add((int)(100 * Math.pow(r.nextDouble(), 3))); // Amount
+            if(a.get(2) > maxMagnitude)
+                maxMagnitude = a.get(2);
+            a.add(r.nextInt(100000));
+            randomData.add(a);
         }
         length = spot + 50;
 
@@ -62,26 +62,26 @@ public class VisualizerPanel extends JComponent {
     }
     
     private int getWholeWidth() {
-    	return getFigureWidth() + (2 * getTextColumnWidth()); // Add a margin between text columns and figure
+        return (2 * margin) + getFigureWidth() + (2 * getTextColumnWidth()); // Add a margin between text columns and figure
     }
     
     private int getWholeHeight() {
-    	return getFigureWidth() + (2 * getTextColumnWidth()); // Add a margin between text columns and figure
+        return (2 * margin) + getFigureWidth() + (2 * getTextColumnWidth()); // Add a margin between text columns and figure
     }
     
     private int getTextColumnWidth() {
-    	return 100; // Find max label width
+        return 100; // Find max label width
     }
 
     private int getFigureWidth() {
-    	return (int)((2*margin) + ((diameter + maxMagnitude) * zoom));
+        return (int)((diameter + maxMagnitude) * zoom);
     }
     
     public void paint(Graphics graphics) {
         super.paint(graphics);
         Graphics2D g = (Graphics2D)graphics;
         int zoomdiam = (int)(diameter * zoom);
-        int centerx = getTextColumnWidth() + getFigureWidth()/2;
+        int centerx = getWholeWidth()/2;
         int centery = getWholeHeight()/2;
         int left = centerx - (zoomdiam/2);
         int top = centery - (zoomdiam/2);
@@ -100,20 +100,20 @@ public class VisualizerPanel extends JComponent {
         double endDeg;
         double magnitude;
         for (int i = 0; i < randomData.size(); i++) {
-        	startDeg = 360 * randomData.get(i).get(0) / length;
-        	endDeg = 360 * randomData.get(i).get(1) / length;
-        	
-        	magnitude = randomData.get(i).get(2) * zoom;
-        	
-        	g.fill(
-        			new Arc2D.Double(
-        					left - magnitude/2,
-        					top  - magnitude/2,
-        					zoomdiam + magnitude,
-        					zoomdiam + magnitude,
-        					startDeg,
-        					endDeg - startDeg,
-        					Arc2D.PIE));
+            startDeg = 360 * randomData.get(i).get(0) / length;
+            endDeg = 360 * randomData.get(i).get(1) / length;
+
+            magnitude = randomData.get(i).get(2) * zoom;
+
+            g.fill(
+                    new Arc2D.Double(
+                            left - magnitude/2,
+                            top  - magnitude/2,
+                            zoomdiam + magnitude,
+                            zoomdiam + magnitude,
+                            90 - startDeg,
+                            -(endDeg - startDeg),
+                            Arc2D.PIE));
         }
         g.setColor(Color.white);
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
@@ -126,16 +126,29 @@ public class VisualizerPanel extends JComponent {
         
         FontMetrics fm = g.getFontMetrics();
         Rectangle2D bounds;
-        int rightcolumn_right = margin + 100;
-        int rightcolumn_top = margin;
-        int pos = rightcolumn_top;
-        for (int i = 0; i < randomData.size(); i++) {
-        	bounds = fm.getStringBounds(randomData.get(i).get(3).toString(), g);
-        	g.drawString(
-        			randomData.get(i).get(3).toString(),
-        			(int)(rightcolumn_right - bounds.getWidth()),
-        			(int)(pos + bounds.getHeight()));
-        	pos = pos + (int)bounds.getHeight();
+        int rightcolumn_left = margin + 100 + getFigureWidth();
+        int column_top = margin;
+        int leftcolumn_right = margin + 100;
+        int pos = column_top;
+        int i = 0;
+        for (; i < randomData.size(); i++) {
+            if ((randomData.get(i).get(0) + randomData.get(i).get(1))/2 > length/2)
+                break;
+            bounds = fm.getStringBounds(randomData.get(i).get(3).toString(), g);
+            g.drawString(
+                    randomData.get(i).get(3).toString(),
+                    rightcolumn_left,
+                    (int)(pos + bounds.getHeight()));
+            pos = pos + (int)bounds.getHeight();
+        }
+        pos = column_top;
+        for (; i < randomData.size(); i++) {
+            bounds = fm.getStringBounds(randomData.get(i).get(3).toString(), g);
+            g.drawString(
+                    randomData.get(i).get(3).toString(),
+                    (int)(leftcolumn_right - bounds.getWidth()),
+                    (int)(pos + bounds.getHeight()));
+            pos = pos + (int)bounds.getHeight();
         }
     }
 }
